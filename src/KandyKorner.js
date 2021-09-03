5916d7bf9411226d3db7f6e0817805285226be10
Blake Rhoden <zbrhoden@gmail.com>
2021-09-03 10:10:11 -0500

    redo

---
+++ src/KandyKorner.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { LocationList } from "./Locations/LocationList"
+
+export const Repairs = () => {
+    return (
+        <>
+        <h1>KandyKorner's Kandy Store</h1>
+        
+        <h2>Customer List</h2>
+        <LocationList />
+        </>
+    )
+}
---
+++ src/LocationList/LocationList.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react"
+
+export const LocationList = () => {
+    const [locations, setlocations] = useState([])
+
+    useEffect(
+        () => {
+            fetch("http://localhost:8088/locations")
+                .then(res => res.json())
+                .then((locationArray) => {
+                    setlocations(locationArray)
+                })
+        },
+        []
+    )
+
+    return (
+        <>
+            {
+                locations.slice(0, 5).map(
+                    (locationObject) => {
+                        return <h2 key={`location--${locationObject.id}`}>{locationObject.name}</h2>
+                    }
+                )
+            }
+        </>
+    )
+}
